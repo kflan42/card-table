dd64719298dbba59ff16b95b0e5fdcbba1f234bb
kerran <29559579+kflan42@users.noreply.github.com>
2020-05-19 02:10:40 -0400

    remove unused files, add TODO list from first game night!

--- dnd_tutorial/src/Constants.tsx
+++
@@ -1,7 +0,0 @@
-// Drag sources and drop targets only interact
-// if they have the same string type.
-// You want to keep types in a separate file with
-// the rest of your app's constants.
-export const ItemTypes = {
-    CARD: 'card',
-  }
