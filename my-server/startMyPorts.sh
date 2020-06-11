5486ac0288ea8e2425f8ea9609a1bc276bcc5211
kerran <29559579+kflan42@users.noreply.github.com>
2020-06-11 00:11:57 -0400

    new todos from running the game tonight
     - cmd to run game from windows shell and log

--- my-server/startMyPorts.sh
+++ my-server/startMyPorts.sh
@@ -1,1 +1,2 @@
-python3 hello.py --port 17172 --static_port 17171 --public_ip skiingbeast.hopto.org
+# windows recognizes python, bash needs python3
+python hello.py --port 17172 --static_port 17171 --public_ip skiingbeast.hopto.org > game1.log 2>&1
