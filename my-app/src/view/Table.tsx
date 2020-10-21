a7b8af1def44f9b68ccd071ef711f83b919cdd18
kerran <29559579+kflan42@users.noreply.github.com>
2020-10-21 00:31:59 -0400

    consistently snake playmats across 2 rows

--- my-app/src/view/Table.tsx
+++ my-app/src/view/Table.tsx
@@ -18,16 +18,20 @@ const Table: React.FC = () => {
     });
 
     const shownPlayers = Object.keys(playersState).filter(p => !hiddenPlayers.includes(p))
-    const mats = []
+    const row1: JSX.Element[] = []
+    const row2: JSX.Element[] = []
     const playersPerRow = Math.ceil(shownPlayers.length/2)
     const width = (1.0 - 0.008 * playersPerRow) / playersPerRow // margins and borders
     for (const player of shownPlayers) {
-        mats.push(<Playmat key={player} player={player} width={width} />)
+        const row = row1.length < playersPerRow ? row1 : row2
+        row.push(<Playmat key={player} player={player} width={width} />)
     }
 
+
     return (
-        <div className="Table" style={{flexDirection: "row"}}>
-            {mats}
+        <div className="Table">
+            <div className="TableRow">{row1}</div>
+            <div className="TableRow" style={{flexDirection:"row-reverse"}}>{row2}</div>
         </div>
     )
 }
